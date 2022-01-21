2ff8ce6a9ba0614db2558cad7e4dce9cd30971ca
gre1pk <raik1996@gmail.com>
2022-01-21 22:45:32 +0300

    feat: brends swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
 import "../scss/style.scss";
+import Swiper, { Pagination, Navigation } from "swiper";
 
 const burgerOpen = document.querySelector(".page-header__burger");
 const burgerClose = document.querySelector(".burger-menu__close");
 const burgermMenu = document.querySelector(".burger-menu");
 const body = document.body;
-let { clientWidth } = document.body;
 
-
+//burger
 burgerOpen.onclick = () => {
   burgermMenu.classList.add("active");
 };
@@ -24,3 +24,53 @@ body.onclick = ({ target }) => {
     burgermMenu.classList.remove("active");
   }
 };
+
+// slider
+Swiper.use([Navigation, Pagination]);
+
+let catalogSlider = null;
+const mediaQuerySize = 767;
+
+function catalogSliderInit() {
+  if (!catalogSlider) {
+    catalogSlider = new Swiper(".swiper", {
+      slidesPerView: "auto",
+      spaceBetween: 30,
+      // Optional parameters
+      loop: true,
+
+      // If we need pagination
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      // And if we need scrollbar
+      scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+    });
+  }
+}
+
+function catalogSliderDestroy() {
+  if (catalogSlider) {
+    catalogSlider.destroy();
+    catalogSlider = null;
+  }
+}
+
+window.addEventListener("resize", renderSwiper);
+
+renderSwiper();
+
+function renderSwiper() {
+  let windowWidth = document.body.clientWidth;
+
+  // Если ширина экрана меньше или равна mediaQuerySize(1024)
+  if (windowWidth <= mediaQuerySize) {
+    // Инициализировать слайдер если он ещё не был инициализирован
+    catalogSliderInit();
+  } else {
+    // Уничтожить слайдер если он был инициализирован
+    catalogSliderDestroy();
+  }
+}
