34d4c9c0e3ff27159cd3fc837067852209ca1f29
gre1pk <raik1996@gmail.com>
2022-01-23 15:17:55 +0300

    feat: add more button

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,8 @@ const burgerOpen = document.querySelector(".page-header__burger");
 const burgerClose = document.querySelector(".burger-menu__close");
 const burgermMenu = document.querySelector(".burger-menu");
 const body = document.body;
-
+const brendsMore = document.querySelector(".brends__more");
+const brendsList = document.querySelector(".brends__slide-list");
 //burger
 burgerOpen.onclick = () => {
   burgermMenu.classList.add("active");
@@ -35,7 +36,7 @@ function catalogSliderInit() {
   if (!catalogSlider) {
     catalogSlider = new Swiper(".swiper", {
       slidesPerView: "auto",
-      spaceBetween: 30,
+      spaceBetween: 16,
       // Optional parameters
       loop: true,
 
@@ -72,5 +73,39 @@ function renderSwiper() {
   } else {
     // Уничтожить слайдер если он был инициализирован
     catalogSliderDestroy();
+  }
+}
+
+// brends more
+
+brendsMore.onclick = () => {
+  if (brendsMore.classList.contains("more-button__close")) {
+    brendsMore.textContent = "Показать все";
+    brendsMore.classList.remove("more-button__close");
+    const itemList = document.querySelectorAll(".clone-item");
+    removeMore(itemList);
+  } else {
+    brendsMore.textContent = "Скрыть";
+    brendsMore.classList.add("more-button__close");
+    addMoreBrends();
+  }
+};
+
+function addMoreBrends() {
+  const brendsClone = brendsList.cloneNode(true);
+  // brendsClone.classList.add("asdasda");
+
+  const brendsItems = brendsClone.children;
+  for (let i = 0; i < brendsItems.length; i++) {
+    const li = brendsItems[i];
+    li.classList.add("clone-item");
+    brendsList.appendChild(li);
+  }
+}
+
+//уневерсальная функция удаления добавленный элементов /сделать так-же к добавить
+function removeMore(itemList) {
+  for (let i = 0; i < itemList.length; i++) {
+    itemList[i].remove();
   }
 }
