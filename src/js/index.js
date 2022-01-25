307d27cbeeef8ded6f85db7f408298078e06db02
gre1pk <raik1996@gmail.com>
2022-01-26 01:36:51 +0300

    fix: array on function

--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,7 @@ const burgerClose = document.querySelector(".burger-menu__close");
 const burgermMenu = document.querySelector(".burger-menu");
 const body = document.body;
 const brendsMore = document.querySelector(".brends__more");
-const brendsList = document.querySelector(".brends__slide-list");
 const technicsMore = document.querySelector(".technics__more");
-const technicsList = document.querySelector(".technics__slide-list");
 
 //burger
 burgerOpen.onclick = () => {
@@ -30,12 +28,12 @@ body.onclick = ({ target }) => {
 };
 
 // slider
+
 Swiper.use([Navigation, Pagination]);
 
 let catalogSlider = null;
-const mediaQuerySize = 767;
 
-function catalogSliderInit() {
+const catalogSliderInit = () => {
   if (!catalogSlider) {
     catalogSlider = new Swiper(".swiper", {
       slidesPerView: "auto",
@@ -53,25 +51,12 @@ function catalogSliderInit() {
       },
     });
   }
-}
+};
 
-function catalogSliderDestroy() {
-  if (catalogSlider) {
-    catalogSlider.forEach((e) => {
-      e.destroy();
-    });
-    catalogSlider = null;
-  }
-}
-
-window.addEventListener("resize", renderSwiper);
-
-renderSwiper();
-
-function renderSwiper() {
+const renderSwiper = () => {
   let windowWidth = document.body.clientWidth;
+  const mediaQuerySize = 767;
 
-  // Если ширина экрана меньше или равна mediaQuerySize(1024)
   if (windowWidth <= mediaQuerySize) {
     // Инициализировать слайдер если он ещё не был инициализирован
     catalogSliderInit();
@@ -79,22 +64,37 @@ function renderSwiper() {
     // Уничтожить слайдер если он был инициализирован
     catalogSliderDestroy();
   }
-}
+};
+
+const catalogSliderDestroy = () => {
+  if (catalogSlider) {
+    catalogSlider.forEach((e) => {
+      e.destroy();
+    });
+    catalogSlider = null;
+  }
+};
+
+window.addEventListener("resize", renderSwiper);
+
+renderSwiper();
 
 // technics more
 technicsMore.onclick = () => {
-  togleMore(technicsMore, "tehnic");
+  togleMoreHandler(technicsMore, "tehnic");
 };
 
 // brends more
 
 brendsMore.onclick = () => {
-  togleMore(brendsMore, "brend");
+  togleMoreHandler(brendsMore, "brend");
 };
 
 // add item list
 
-function addMoreBrends(name) {
+const showMore = (name) => {
+  const brendsList = document.querySelector(".brends__slide-list");
+  const technicsList = document.querySelector(".technics__slide-list");
   let list;
   if (name === "tehnic") {
     list = technicsList;
@@ -102,34 +102,33 @@ function addMoreBrends(name) {
   if (name === "brend") {
     list = brendsList;
   }
-
-  const brendsClone = list.cloneNode(true);
-  const brendsItems = brendsClone.children;
-  for (let i = 0; i < brendsItems.length; i++) {
-    const li = brendsItems[i];
+  const listClone = list.cloneNode(true);
+  const listItems = listClone.children;
+  for (let i = 0; i < listItems.length; i++) {
+    const li = listItems[i];
     li.classList.add("clone-item");
     list.appendChild(li);
   }
-}
+};
 
 // remove item list
 
-function removeMore(itemList) {
+const removeMoreHandler = (itemList) => {
   for (let i = 0; i < itemList.length; i++) {
     itemList[i].remove();
   }
-}
+};
 
 // togle more button
-function togleMore(button, name) {
+const togleMoreHandler = (button, name) => {
   if (button.classList.contains("more-button__close")) {
     button.textContent = "Показать все";
     button.classList.remove("more-button__close");
     const itemList = document.querySelectorAll(".clone-item");
-    removeMore(itemList);
+    removeMoreHandler(itemList);
   } else {
     button.textContent = "Скрыть";
     button.classList.add("more-button__close");
-    addMoreBrends(name);
+    showMore(name);
   }
-}
+};
